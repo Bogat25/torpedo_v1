2b71e87b829b3e4548efeb225c7df2c962d1e068
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2023-12-09 19:55:19 +0100

    kisebb változások, az egész rendszer át lessz dolgozva egy sokkal letisztultabra

--- hajo_helyvalasztas.c
+++ hajo_helyvalasztas.c
@@ -12,59 +12,72 @@ void hajo_helyvalasztas(players *player)
             int hajo_lerakhato = 0;
             while (hajo_lerakhato != 1)
             {
-            printf("A(z) %d egyseg hosszu hajo helyzete/iranya:\n", hajo_meret);
-            int x = 0;
-            int y = 0;
-            int irany = -1;
-            printf("X: ");
-            scanf("%d", &x);
-            printf("Y: ");
-            scanf("%d", &y);
-            while (irany < 0 || irany > 1)
-            {
-            printf("Irany (0 = visszintes, 1 = fuggoleges): ");
-            scanf("%d", &irany);   
-            }
+                printf("A(z) %d egyseg hosszu hajo helyzete/iranya:\n", hajo_meret);
+                int x = 0;
+                int y = 0;
+                int irany = -1;
+                printf("X: ");
+                scanf("%d", &x);
+                printf("Y: ");
+                scanf("%d", &y);
+                while (irany < 0 || irany > 1)
+                {
+                    printf("Irany (0 = visszintes, 1 = fuggoleges): ");
+                    scanf("%d", &irany);
+                }
 
-            if (x > tabla_meret || x < 0|| y > tabla_meret || y < 0)
-            {
-                printf(RED_TEXT"Rossz helyre lett megadva a kezdopont.\n"); printf(RESET_TEXT);
-                hajo_meret++;
-                break;
-            }
-            if (irany == 0 && y + hajo_meret > tabla_meret || irany == 1 && x + hajo_meret > tabla_meret)
-            {
-                printf(RED_TEXT"A hajo kilog a palyarol.\n"); printf(RESET_TEXT);
-                hajo_meret++;
-                break;
-            }
-            if (x > 1)
-            {
-                for (int i = y - 1; i < y + hajo_meret +1; i++)
+                if (x > tabla_meret || x < 0 || y > tabla_meret || y < 0)
                 {
-                    if (player[i].matrix[x][i] == 1)
+                    printf(RED_TEXT "Rossz helyre lett megadva a kezdopont.\n");
+                    printf(RESET_TEXT);
+                    hajo_meret++;
+                    break;
+                }
+                if (irany == 0 && y + hajo_meret > tabla_meret || irany == 1 && x + hajo_meret > tabla_meret)
+                {
+                    printf(RED_TEXT "A hajo kilog a palyarol.\n");
+                    printf(RESET_TEXT);
+                    hajo_meret++;
+                    break;
+                }
+                if (x > 1 && irany == 0)
+                {
+                    for (int i = y - 1; i < y + hajo_meret + 1; i++)
                     {
-                        printf(RED_TEXT"Ide nem rakhatod mivel utkozne egy másik hajoval\n"); printf(RESET_TEXT);
+                        if (player[i].matrix[x][i] == 1)
+                        {
+                            printf(RED_TEXT "Ide nem rakhatod mivel utkozne egy másik hajoval\n");
+                            printf(RESET_TEXT);
+                        }
+                        hajo_meret++;
+                        break;
                     }
-                    
+                }
+                if (x < tabla_meret - 1 && irany == 0)
+                {
+                    for (int i = y - 1; i < y + hajo_meret + 1; i++)
+                    {
+                        if (player[i].matrix[x + 1][i] == 1)
+                        {
+                            printf(RED_TEXT "Ide nem rakhatod mivel utkozne egy másik hajoval\n");
+                            printf(RESET_TEXT);
+                        }
+                        hajo_meret++;
+                        break;
+                    }
+                }
+                if (irany == 0 && y > 1)
+                {
+                    if (player[i].matrix[x - 1][y] == 1 || player[i].matrix[x - 1][y + hajo_meret + 1] == 1 )
+                    {
+                            printf(RED_TEXT "Ide nem rakhatod mivel utkozne egy másik hajoval\n");
+                    }
+                    hajo_meret++;
+                    break;
                 }
                 
-            }
-            if (x < tabla_meret)
-            {
-                for (int i = y - 1; i < y + hajo_meret +1; i++)
-                {
-                    if (player[i].matrix[x][i] == 1)
-                    {
-                        printf(RED_TEXT"Ide nem rakhatod mivel utkozne egy másik hajoval\n"); printf(RESET_TEXT);
-                    }
-                    
-                }
-                
-            }
-            
-            
-            break;
+
+                break;
             }
         }
     }
