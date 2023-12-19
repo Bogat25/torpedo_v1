f824f541c1bdb29389a9383f97f09a2d29d58264
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2023-12-20 00:16:58 +0100

    buggkeresés még mindig, egy új kiiratási módszer

--- hajo_helyvalasztas.c
+++ hajo_helyvalasztas.c
@@ -38,11 +38,15 @@ void hajo_helyvalasztas(players *player)
                     {
                         for (int j = 0; j < 3; j++)
                         {
-                            printf(CYAN_TEXT"x: %d y: %d\n",pozicio_x + j - 1, pozicio_y + i - 1); printf(RESET_TEXT);
                             if (player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1] != 0)
                             {
                                 megfelel = 0;
                                 break;
+                            }
+                            if (player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1] == 0)
+                            {
+                                printf(CYAN_TEXT "x: %d y: %d ertek: %d\n", pozicio_x + j - 1, pozicio_y + i - 1, player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1]);
+                                printf(RESET_TEXT);
                             }
                         }
                     }
@@ -60,7 +64,7 @@ void hajo_helyvalasztas(players *player)
                     }
                 }
                 megfelel = 0;
-                pr_tabla(player);
+                pr_lehejezesek(player);
             }
         }
     }
--- print.c
+++ print.c
@@ -52,7 +52,52 @@ void pr_tabla(players *player)
         pr_terkoz();
         printf(YELLOW_TEXT "%d ", i);
         printf(RESET_TEXT); // 2. player
-        for (int n = 1; n < tabla_meret +1; n++)
+        for (int n = 1; n < tabla_meret + 1; n++)
+        {
+            printf("%d ", player[1].matrix[i][n]);
+        }
+        printf("\n");
+    }
+}
+
+void pr_lehejezesek(players *player)
+{
+    printf("%s", player[0].nev);
+    int nev_hossz = strlen(player[0].nev); // szoköz a playerek nevei között
+    for (int i = 0; i < tabla_meret + 2 * terkoz - nev_hossz; i++)
+    {
+        printf(" ");
+    }
+
+    printf("%s", player[1].nev);
+    printf("\n");
+    printf("  "); // 1. player
+    for (int i = 0; i < tabla_meret + 2; i++)
+    {
+        printf(CYAN_TEXT "%d ", i);
+        printf(RESET_TEXT);
+    }
+    pr_terkoz();
+    printf("  "); // 2. player
+    for (int i = 0; i < tabla_meret + 2; i++)
+    {
+        printf(CYAN_TEXT "%d ", i);
+        printf(RESET_TEXT);
+    }
+
+    printf("\n");
+    for (int i = 1; i < tabla_meret + 2; i++)
+    {
+        printf(YELLOW_TEXT "%d ", i);
+        printf(RESET_TEXT); // 1. player
+        for (int n = 1; n < tabla_meret + 2; n++)
+        {
+            printf("%d ", player[0].matrix[i][n]);
+        }
+        pr_terkoz();
+        printf(YELLOW_TEXT "%d ", i);
+        printf(RESET_TEXT); // 2. player
+        for (int n = 1; n < tabla_meret + 2; n++)
         {
             printf("%d ", player[1].matrix[i][n]);
         }
