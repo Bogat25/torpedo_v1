c67f883221a598e6fce20de95afdcf7af121a707
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2023-12-19 22:42:39 +0100

    a iszintes hajokat már nagyából jól kezeli a program, még hiányzik elég sok minden a kodból

--- hajo_helyvalasztas.c
+++ hajo_helyvalasztas.c
@@ -12,38 +12,51 @@ void hajo_helyvalasztas(players *player)
     int megfelel = 0;
     while (megfelel == 0)
     {
-        for (int l = 1; l < 6; l++)
+        for (int hanyadik_jatekos = 0; hanyadik_jatekos < db_jatekos; hanyadik_jatekos++)
         {
-            hajo_meret = l;
-            pozicio_x = 0;
-            pozicio_y = 0;
-            while (pozicio_x < 1 || pozicio_x > 8 || pozicio_y < 1 || pozicio_y > 8 || tabla_meret - pozicio_y < hajo_meret - 1)
+            for (int l = 1; l < 6; l++)
             {
-                printf("meret: %d", hajo_meret);
-                printf("\n");
-                printf("x: ");
-                scanf("%d", &pozicio_x);
-                printf("y: ");
-                scanf("%d", &pozicio_y);
-                printf("\n");
-            }
-            pozicio_x++;
-            pozicio_y++;
-            for (int i = 0; i < hajo_meret + 2; i++)
-            {
-                for (int j = 0; j < 3; j++)
+                hajo_meret = l;
+                pozicio_x = 0;
+                pozicio_y = 0;
+                while (megfelel != 1)
                 {
-                    if (player[0].matrix[pozicio_x - 1 + j][pozicio_y - 1 + i] != 0)
+                    while (pozicio_x < 1 || pozicio_x > 8 || pozicio_y < 1 || pozicio_y > 8 || tabla_meret - pozicio_y < hajo_meret - 1)
                     {
-                        printf(RED_TEXT "X %d", player[0].matrix[pozicio_x - 1 + j][pozicio_y - 1 + i]);
-                        printf(RESET_TEXT);
+                        printf("meret: %d", hajo_meret);
+                        printf("\n");
+                        printf("x: ");
+                        scanf("%d", &pozicio_x);
+                        printf("y: ");
+                        scanf("%d", &pozicio_y);
+                        printf("\n");
                     }
-                    if (player[0].matrix[pozicio_x - 1 + j][pozicio_y - 1 + i] == 0)
+                    pozicio_x++;
+                    pozicio_y++;
+                    megfelel = 1;
+                    for (int i = 0; i < hajo_meret + 2 && megfelel == 1; i++)
                     {
-                        printf(GREEN_TEXT "M %d", player[0].matrix[pozicio_x - 1 + j][pozicio_y - 1 + i]);
-                        printf(RESET_TEXT);
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (player[hanyadik_jatekos].matrix[pozicio_x - 1 + j][pozicio_y - 1 + i] != 0)
+                            {
+                                printf(RED_TEXT "X %d", player[hanyadik_jatekos].matrix[pozicio_x - 1 + j][pozicio_y - 1 + i]);
+                                printf(RESET_TEXT);
+                                megfelel = 0;
+                                break;
+                            }
+                        }
+                    }
+                    if (megfelel == 1)
+                    {
+                        for (int q = 0; q < hajo_meret; q++)
+                        {
+                            player[hanyadik_jatekos].matrix[pozicio_x - 1][pozicio_y + q - 1] = hajo_meret;
+                        }
                     }
                 }
+                megfelel = 0;
+                pr_tabla(player);
             }
         }
     }
