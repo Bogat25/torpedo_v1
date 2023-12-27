517dbb52885e812df2d13059c493597f29306773
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2023-12-27 19:04:15 +0100

    helyfoglalas befejezve

--- hajo_helyvalasztas.c
+++ hajo_helyvalasztas.c
@@ -8,6 +8,7 @@ void hajo_helyvalasztas(players *player)
     int player_kovetkezik = 1;
     int pozicio_x = 0; // bal fentről számmolom
     int pozicio_y = 0;
+    int irany = 0; // 0 => visszintes, 1 => föggőleges
     int hajo_meret = 5;
     int megfelel = 0;
     while (megfelel == 0)
@@ -21,10 +22,14 @@ void hajo_helyvalasztas(players *player)
                 {
                     pozicio_x = 0;
                     pozicio_y = 0;
-                    while (pozicio_x < 1 || pozicio_x > 8 || pozicio_y < 1 || pozicio_y > 8 || tabla_meret - pozicio_y < hajo_meret - 1)
+                    irany = -1;
+                    while (irany == 0 && (pozicio_x < 1 || pozicio_x > 8 || pozicio_y < 1 || pozicio_y > 8 || tabla_meret - pozicio_y < hajo_meret - 1) ||
+                           irany == 1 && (pozicio_x < 1 || pozicio_x > 8 || pozicio_y < 1 || pozicio_y > 8 || tabla_meret - pozicio_x < hajo_meret - 1) || irany < 0 || irany > 1)
                     {
                         printf("Meret: %d", hajo_meret);
                         printf("\n");
+                        printf("Irany, (1 = fuggoleges, 0 = visszintes): ");
+                        scanf("%d", &irany);
                         printf("x: ");
                         scanf("%d", &pozicio_x);
                         printf("y: ");
@@ -34,42 +39,63 @@ void hajo_helyvalasztas(players *player)
                     pozicio_x++;
                     pozicio_y++;
                     megfelel = 1;
-                    for (int i = 0; i < hajo_meret + 2 && megfelel == 1; i++)
+                    if (irany == 0)
                     {
-                        for (int j = 0; j < 3; j++)
+                        for (int i = 0; i < hajo_meret + 2 && megfelel == 1; i++)
                         {
-                            if (player[hanyadik_jatekos].matrix[pozicio_x + j - 2][pozicio_y + i - 2] != 0)
+                            for (int j = 0; j < 3; j++)
                             {
-                                printf(RED_TEXT "Nem felel meg 1!!!");
-                                printf(RESET_TEXT);
-                                megfelel = 0;
-                                break;
+                                if (player[hanyadik_jatekos].matrix[pozicio_x + j - 2][pozicio_y + i - 2] != 0)
+                                {
+                                    printf(RESET_TEXT);
+                                    megfelel = 0;
+                                    break;
+                                }
                             }
-                            /*                                                                                      Debuggoláshoz használtam
-                            if (player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1] == 0)
+                        }
+                        if (megfelel == 0)
+                        {
+                            printf(RED_TEXT "Utkozes tortent\n");
+                            printf(RESET_TEXT);
+                        }
+                        else if (megfelel == 1)
+                        {
+                            for (int q = 0; q < hajo_meret; q++)
                             {
-                                printf(CYAN_TEXT "x: %d y: %d ertek: %d\n", pozicio_x + j - 1, pozicio_y + i - 1,
-                                       player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1]);
-                                printf(RESET_TEXT);
+                                player[hanyadik_jatekos].matrix[pozicio_x - 1][pozicio_y + q - 1] = hajo_meret;
                             }
-                            */
                         }
                     }
-                    if (megfelel == 0)
+                    if (irany == 1)
                     {
-                        printf(RED_TEXT "Utkozes tortent\n");
-                        printf(RESET_TEXT);
-                    }
-                    else if (megfelel == 1)
-                    {
-                        for (int q = 0; q < hajo_meret; q++)
+                        for (int i = 0; i < hajo_meret + 2 && megfelel == 1; i++)
                         {
-                            player[hanyadik_jatekos].matrix[pozicio_x - 1][pozicio_y + q - 1] = hajo_meret;
+                            for (int j = 0; j < 3; j++)
+                            {
+                                if (player[hanyadik_jatekos].matrix[pozicio_x + i - 2][pozicio_y + j - 2] != 0)
+                                {
+                                    printf(RESET_TEXT);
+                                    megfelel = 0;
+                                    break;
+                                }
+                            }
+                        }
+                        if (megfelel == 0)
+                        {
+                            printf(RED_TEXT "Utkozes tortent\n");
+                            printf(RESET_TEXT);
+                        }
+                        else if (megfelel == 1)
+                        {
+                            for (int q = 0; q < hajo_meret; q++)
+                            {
+                                player[hanyadik_jatekos].matrix[pozicio_x + q - 1][pozicio_y- 1] = hajo_meret;
+                            }
                         }
                     }
                 }
                 megfelel = 0;
-                pr_lehejezesek(player);
+                pr_tabla(player);
             }
         }
     }
---
+++ not_used_code.c
@@ -0,0 +1,9 @@
+//Hajó helyválasztásból, debuggoláshoz használtam
+/*
+if (player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1] == 0)
+{
+    printf(CYAN_TEXT "x: %d y: %d ertek: %d\n", pozicio_x + j - 1, pozicio_y + i - 1,
+           player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1]);
+    printf(RESET_TEXT);
+}
+*/
