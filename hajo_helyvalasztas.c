db399644ea679129865bde2440ae3bb5a470e534
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2023-12-27 18:38:07 +0100

    a visszintes lehelyezés megodva (elvileg, még tesztelem)

--- hajo_helyvalasztas.c
+++ hajo_helyvalasztas.c
@@ -38,17 +38,21 @@ void hajo_helyvalasztas(players *player)
                     {
                         for (int j = 0; j < 3; j++)
                         {
-                            if (player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1] != 0)
+                            if (player[hanyadik_jatekos].matrix[pozicio_x + j - 2][pozicio_y + i - 2] != 0)
                             {
-                                printf(RED_TEXT"Nem felel meg 1!!!");printf(RESET_TEXT);
+                                printf(RED_TEXT "Nem felel meg 1!!!");
+                                printf(RESET_TEXT);
                                 megfelel = 0;
                                 break;
                             }
+                            /*                                                                                      Debuggoláshoz használtam
                             if (player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1] == 0)
                             {
-                                printf(CYAN_TEXT "x: %d y: %d ertek: %d\n", pozicio_x + j - 1, pozicio_y + i - 1, player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1]);
+                                printf(CYAN_TEXT "x: %d y: %d ertek: %d\n", pozicio_x + j - 1, pozicio_y + i - 1,
+                                       player[hanyadik_jatekos].matrix[pozicio_x + j - 1][pozicio_y + i - 1]);
                                 printf(RESET_TEXT);
                             }
+                            */
                         }
                     }
                     if (megfelel == 0)
